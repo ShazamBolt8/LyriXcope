3b4810b889f8233208263ae9aacf5131b83aac3e
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-05-30 04:41:53 +0500

    minor fixes

--- test.js
+++
@@ -1,9 +0,0 @@
-const { configureLyrixcope, getSongLink, scrapeLyrics, getSongLyrics } = require("./main.js");
-
-async function main() {
-  configureLyrixcope("12520", "zHw9vAG7r8LlrY5u");
-  const songData = await getSongLyrics("a man without love", null);
-  console.log(songData);
-}
-
-main();
